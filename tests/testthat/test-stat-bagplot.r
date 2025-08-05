df6c7782e93f83c70c1cf43b11d06ffabc6c712a
Jason Cory Brunson <cornelioid@gmail.com>
2025-08-05 16:42:35 -0500

    add parameters and defaults for outlier label text - fixes #14

--- tests/testthat/test-stat-bagplot.r
+++ tests/testthat/test-stat-bagplot.r
@@ -5,15 +5,22 @@ test_that("`stat_bagplot()` doesn't throw errors when elements are missing", {
   expect_equal(layer_grob(p0)[[1L]], zeroGrob())
 })
 
+t <- seq(0, 2/3, 1/3) * 2*pi
+d1 <- data.frame(x = cos(t), y = sin(t))
+d2 <- data.frame(x = 2*cos(t+1/3*pi), y = 2*sin(t+1/3*pi))
+d3 <- rbind(d1, d2)
+d3 <- transform(d3, id = LETTERS[seq(nrow(d3))])
+
 test_that("`stat_bagplot()` encodes all plot elements unless no outliers", {
-  t <- seq(0, 2/3, 1/3) * 2*pi
-  d1 <- data.frame(x = cos(t), y = sin(t))
   expect_no_error(p1 <- ggplot(d1, aes(x, y)) + stat_bagplot())
   expect_setequal(unique(layer_data(p1)$component), c("median", "bag", "fence"))
-  
-  d2 <- data.frame(x = 2*cos(t+1/3*pi), y = 2*sin(t+1/3*pi))
-  d3 <- rbind(d1, d2)
   expect_no_error(p3 <- ggplot(d3, aes(x, y)) + stat_bagplot(coef = 1))
   expect_setequal(unique(layer_data(p3)$component),
                   c("median", "bag", "fence", "outliers"))
+})
+
+test_that("`stat_bagplot()` retains position and (outlier) label aesthetics", {
+  expect_no_error(p3 <- ggplot(d3, aes(x, y, label = id)) + stat_bagplot())
+  l3 <- layer_data(p3)
+  expect_equal(which(! is.na(l3$label)), which(l3$component == "outliers"))
 })
