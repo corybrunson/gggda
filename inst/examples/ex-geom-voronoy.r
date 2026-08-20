ef44d303ac669cf3648f2dc0ff85463afa2a4463
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2026-08-20 13:08:27 -0400

    use square coord to aid geom examples

--- inst/examples/ex-geom-voronoy.r
+++ inst/examples/ex-geom-voronoy.r
@@ -7,12 +7,12 @@ usa_mds$coastal <- c(rep(FALSE, 4L), rep(TRUE, 6L))
 # polygon-based rendering (cell interiors and perimeter paths)
 usa_mds %>%
   ggplot(aes(-V1, -V2, label = city)) +
-  coord_equal() +
+  coord_square() +
   geom_voronoy(aes(fill = coastal), colour = NA) +
   geom_text(size = 3)
 # segment-based rendering (de-duplicated cell boundaries)
 usa_mds %>%
   ggplot(aes(-V1, -V2, label = city)) +
-  coord_equal() +
+  coord_square() +
   geom_thiessen(aes(colour = coastal)) +
   geom_text(size = 3)
